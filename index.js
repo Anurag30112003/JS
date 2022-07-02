b297ab500db7f1849c7b7e9b00e9e7834ff84c7e
Anurag <anuragsharma011011@gmail.com>
2022-07-03 02:31:09 +0530

    commit #1

---
+++ index.js
@@ -0,0 +1,115 @@
+// console.log('Hello World');
+
+// let name = 'Rick'; // String
+
+// let age = 25; // Number
+
+// let isApproved = false; // Boolean
+
+// let firstName = undefined; // Undefined
+
+// let selectedColour = null; // Null
+
+
+// let num1 = 5
+// let num2 = 5
+
+// function addNumber(num1 , num2){
+//     return num1 + num2
+
+// }
+
+// console.log(addNumber(num1,num2))
+
+// const fruits = ['Apple','Mango', 'Oranges'];
+
+//  const hello = fruits.map(function (variable){
+//     return variable[2]
+//  })
+
+
+// const todos = [
+//    {
+//       id:1,
+//       text:'Take out Trash',
+//       status: true
+//    },
+//    {
+//       id:2,
+//       text:'Meeting with boss',
+//       status:true
+//    },
+//    {
+//       id:3,
+//       text:'Dentist Appointment',
+//       status:false
+//    }
+// ];
+
+
+// const hell = JSON.stringify(todos);
+//  console.log(hell)
+
+// for (let i = 0; i<todos.length; i++){
+
+//    console.log(todos[i].text , todos[i].status)
+
+
+// }
+
+// for (i of todos){
+//    console.log(i.text)
+// };
+
+// todos.forEach(function(todo){
+//    console.log(todo.text)
+// });
+
+// const todoItem = todos.map(function(todo){
+//    // console.log( todo.text)
+//    return todo.text
+// });
+
+// console.log(todoItem)
+
+// const todoCompleted = todos.filter(function(todo){
+//    return todo.status === true
+// });
+// console.log(todoCompleted)
+
+// const todoCompleted = todos.filter(function(todo){
+//    return todo.status === false
+// }).map(function(todo){
+//    return todo.text
+// });
+// console.log(todoCompleted)
+
+// const x = 10;
+
+// if (x === 10){
+//    console.log('X is 10')
+// } else if(x>0){
+//    console.log('x is greater than 0')
+// }else{
+//    console.log('x is smaller than 0')
+// }
+
+// const x = 10;
+// const y = 45;
+// // const color = x==10 && y==5 ? 'red':'blue';
+// const color ='white';
+// // console.log(color)
+
+
+// switch(color){
+//    case 'red':
+//       console.log('colour is red')
+//       break;
+//    case 'blue':
+//       console.log('colour is blue')
+//       break;
+//    default:
+//       console.log('colour is neither red nor blue')
+//       break;   
+
+//    }
